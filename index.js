1f631d8a4dcbdf6f065816493de6bdb4c2239fa2
Aaron Xie <aaronxie@Jennys-iMac.local>
2020-08-09 00:46:32 -0400

    database issue

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-//new for redesign
+require('dotenv').config();
+const fetch = require('node-fetch');
+
+
+const express = require('express');
+const runner = express();
+
+runner.listen(3000,()=>console.log('listening at 3000'));
+runner.use(express.static('public'));
+
+//get small data set
+runner.get('/getMinMovieData/:title',async (request,response)=>{
+    const rawMovie = await fetch(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${request.params.title}`);
+    const movieJson = await rawMovie.json();
+    response.json(movieJson);
+})
+
+//get the large full data set
+runner.get('/getFullMovieData/:imbdID',async (request,response)=>{
+    const rawMovie = await fetch(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${request.params.imbdID}`);
+    const movieJson = await rawMovie.json();
+    response.json(movieJson);
+})
+
+const database = require('nedb');
+const userMovieEntries = new database('userMovieEntries.db'); 
+userMovieEntries.loadDatabase();
+
+runner.post('/pushMovieData',(request,response)=>{
+    userMovieEntries.insert(request.body);
+
+    response.json({
+        message: 'Success',
+    })
+})
+
--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-//new for redesign
-
 
 /*---User Input---*/
 
@@ -14,6 +12,149 @@ function sessionIDinput(e){
 
     console.log('hi');
 }
+
+
+/*---Movie Searching---*/
+let currentDisplayMovies = [];
+
+const movieNameSearch = document.querySelector('#movieNameInput');
+const movieNameForm = document.querySelector('.searchMovie');
+movieNameForm.addEventListener('submit',inputMovie);
+
+async function inputMovie(e){
+    e.preventDefault();
+    const movieData = await getMinMovies();
+    updateMoviePoster(movieData);
+
+
+}
+
+//returns the fetch of basic movie info
+async function getMinMovies(){
+    const raw = await fetch(`/getMinMovieData/${movieNameSearch.value}`);
+    const movieData = await raw.json();
+    return movieData.Search;
+}
+
+const movieList = document.querySelector('.movieList');
+
+function updateMoviePoster(movieData){
+    movieList.innerHTML = '';
+    currentDisplayMovies = [];
+
+    for(let i=0;i<movieData.length;i++){
+        currentDisplayMovies.push(movieData[i]);
+
+        const theMovie = document.createElement('div');
+        theMovie.classList.add('theMovie');
+
+        const movieImage = document.createElement('img');
+        movieImage.classList.add('movieImage');
+        if (movieData[i].Poster == 'N/A'){
+            movieImage.src ='assets/not_found.jpg';
+        }
+        else{
+            movieImage.src = movieData[i].Poster;
+        }
+
+        const description = document.createElement('div');
+        description.classList.add('description');
+
+        const movieName = document.createElement('p');
+        movieName.classList.add('movieName');
+        movieName.textContent = `${movieData[i].Title} (${movieData[i].Year})`;
+
+        const movieSelect = document.createElement('input');
+        movieSelect.classList.add('movieSelect');
+        movieSelect.type = 'checkbox';
+
+        description.append(movieName);
+        description.append(movieSelect);
+        theMovie.append(movieImage);
+        theMovie.append(description);
+        movieList.append(theMovie);
+        
+    }
+
+}
+
+
+
+
+const toWatchCommentForm = document.querySelector('.toWatchCommentForm');
+const toWatchComment = document.querySelector('#toWatchComment');
+
+toWatchCommentForm.addEventListener('submit',addMovieToDataBase);
+
+async function addMovieToDataBase(e){
+    e.preventDefault();
+    //get list of IMBd id of checked movie from currentDisplayMovies and checkboxes
+    const IDList = getCheckedMovies();
+    //request the full data from backend
+    const movieData = await getFullData(IDList);
+    //push to database
+    for (let i=0; i<movieData.length;i++){
+        console.log(movieData[i]);
+        const response = await pushToDataBase(movieData[i]);
+        console.log(response);
+    }
+}
+
+
+
+function getCheckedMovies(){
+    const checkBox = document.querySelectorAll('.movieSelect');
+    //get here as need the search and checkbox-ing to be finished first
+
+    let currentCheckMoviesID = [];
+    for(let i=0; i<checkBox.length;i++){
+        if(checkBox[i].checked){
+            currentCheckMoviesID.push(currentDisplayMovies[i].imdbID);
+        }
+    }
+
+    return currentCheckMoviesID;
+}
+
+async function getFullData(IDList){
+    let arrayOfMovieObj = [];
+    for (let i=0; i<IDList.length;i++){
+        const raw = await fetch(`/getFullMovieData/${IDList[i]}`);
+        const fullMovieData = await raw.json();
+        arrayOfMovieObj.push(fullMovieData);
+    }
+    return arrayOfMovieObj;
+}
+
+async function pushToDataBase(movieObj){
+    const userComment = toWatchComment.value;
+    const dateWatched = 'Not Yet';
+    const route = '/pushMovieData';
+
+    const sentData = {
+        movie: movieObj.Title,
+        comments: userComment,
+        dateWatched: dateWatched,
+    }
+
+
+    const postOptions = {
+        method: 'POST',
+        headers:{
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(sentData),
+    }
+
+    const raw = await fetch(route,postOptions);
+    const response = await raw.json();
+    return response;
+
+
+}
+
+
+/*---to watch list---*/
 
 
 
